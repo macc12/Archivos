6aecbc2c144061189887bb730c89432303df6aef
JHOHAN <JHOHAN@172.25.10.189>
2018-08-03 12:50:26 -0500

    commit inicial del hola mundo

---
+++ src/Modelo/Estudiante.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author JHOHAN
+ */
+public class Estudiante {
+    private String nombre;
+    private int cedula;
+    private int edad;
+
+    public Estudiante(String nombre, int cedula, int edad){
+        this.cedula=cedula;
+        this.edad=edad;
+        this.nombre=nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+ 
+    
+}
