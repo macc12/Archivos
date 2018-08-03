c1317a43cf03d40ad99e1f224a0ade04e1d1b2b0
LabingXEON <LabingXEON@LabingXEON8-PC>
2018-08-03 12:49:35 -0500

    1.0

---
+++ src/Modelo/Matricula.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Marco
+ */
+public class Matricula {
+    private int id;
+    private int codigo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+}
