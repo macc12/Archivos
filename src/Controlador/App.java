6ff49f322aec0956e16d37f51cae900a7fb9ed8d
LabingXEON <LabingXEON@LabingXEON8-PC>
2018-08-03 13:05:12 -0500

    1.0

---
+++ src/Controlador/App.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Estudiante;
+import Modelo.Matricula;
+import java.util.Scanner;
+
+/**
+ *
+ * @author marco
+ */
+public class App {
+    public static void main(String args[]){
+        Scanner tc = new Scanner(System.in);
+        Estudiante estudiante;
+        Matricula matricula;
+        
+        int op=0;
+        do{
+            System.out.println("1. Garegar estudiante\n2. Agregar Curso\n3. Agregar Materia\4.salir");
+            
+        }while();
+        
+    }
+    
+}
