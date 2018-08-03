a5f93b7a6653bbaf64d4153752d17e360cf067d9
JHOHAN <JHOHAN@172.25.10.189>
2018-08-03 13:11:36 -0500

    commit inicial del hola mundo

--- src/Controlador/App.java
+++ src/Controlador/App.java
@@ -13,18 +13,30 @@ import java.util.Scanner;
  *
  * @author marco
  */
+import java.io.*;
 public class App {
     public static void main(String args[]){
         Scanner tc = new Scanner(System.in);
         Estudiante estudiante;
         Matricula matricula;
+        try{
+            
         
+         RandomAccessFile raf = new RandomAccessFile("Estudiantes.txt", "rw");
+         RandomAccessFile raf1 = new RandomAccessFile("Materias.txt", "rw");
+         RandomAccessFile raf2= new RandomAccessFile("cursos.txt", "rw");
+         
+        }catch(Exception e){
+            System.out.println("fallo en los archivos");
+        }
         int op=0;
         do{
             System.out.println("1. Garegar estudiante\n2. Agregar Curso\n3. Agregar Materia\4.salir");
             
         }while();
+               
+    }
+    public void agregarEstudiantes(){
         
     }
-    
 }
